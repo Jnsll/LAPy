5b31be0e9675d1fb87b2a7c04ca6ca791e1f7e44
Jnsll <june.benvegnu-sallou@irisa.fr>
2021-06-23 11:32:55 +0200

    update

---
+++ src/loop_aggregation.py
@@ -0,0 +1,19 @@
+# LA module
+# The goal is to provide a sort of API to use the Loop Aggregation approach on input data
+# module developped in the context of using it on a modflow model
+
+
+
+
+
+
+"""
+    Inputs :
+        Ref input data as a DataFrame 
+        Rate of aggregation
+        function to aggregate ref data
+        
+
+
+"""
+
